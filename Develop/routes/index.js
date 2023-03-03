f2c5c1ded0f84c1ae91aaac9e087f283edc6ba59
ColtMort <balding.fenders.0e@icloud.com>
2023-03-02 17:46:25 -0700

    deploy to heroku

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,8 +1,37 @@
-const express = require('express')
+const index = require('express').Router()
+const {v4: uuidv4} = require('uuid')
 const path = require('path')
-const index = express()
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
-// routers
-index.use('/')
+index.get('/notes', (req, res) =>{
+    readFromFile(path.join(__dirname, '../db/db.json')).then((data) => res.json(JSON.parse(data)))
+})
 
-exports.routes = routes
+index.post('/notes', async (req, res) => {
+    let body = req.body
+    body.id = uuidv4()
+    readAndAppend(body, path.join(__dirname, '../db/db.json'))
+    let updatedDb = await readFromFile(path.join(__dirname, '../db/db.json'))
+    updatedDb = updatedDb.toString('utf8')
+    res.json(updatedDb)
+})
+
+index.delete('/notes/:id', async (req,res) => {
+    let id = req.params.id
+    let data = await readFromFile(path.join(__dirname, '../db/db.json')).then((data) => {return JSON.parse(data)})
+for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    if(element.id === id){
+        data.splice(i, 1)
+        writeToFile(path.join(__dirname, '../db/db.json'), data)
+        console.log('spliced')
+        return res.send(`id: ${id}, was deleted`)
+    }
+
+};
+res.send(`err: id-${id} not found`)
+    console.log(data)
+
+})
+
+exports.index = index
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const path = require('path')
-const {routes} = require('./Develop/routes/index')
+const {index} = require('./Develop/routes/index')
 
-const PORT = process.ent.port || 3001
+const PORT = process.env.port || 3001
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use('/api', routes)
-app.use(express.static('public'))
+app.use('/api', index)
+app.use(express.static('./Develop/public'))
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
 })
 
 app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
 })
 
 app.listen(PORT, () => {
